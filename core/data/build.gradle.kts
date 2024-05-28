9d5fa64392baacd951e143251db63c42d347ca2c
dreamwalker-123 <batordylgyrov@gmail.com>
2024-05-28 14:44:52 +0800

    сохранение перед добавлением юнит тестов

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -5,9 +5,13 @@ plugins {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
+
+//test {
+//    useJUnitPlatform()
+//}
 
 dependencies {
 //    api(project(":core:model"))
@@ -16,6 +20,10 @@ dependencies {
     implementation(project(":core:model"))
 
     implementation(libs.kotlinx.coroutines.core)
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+    testImplementation("org.mockito:mockito-core:4.0.0")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
 
     // DI Hilt for Kotlin library
     implementation(libs.hilt.core)
